de7f4efdc90204a2a08eae8c00878a684ce85e13
Evan Pierce <mrthinger@gmail.com>
2021-08-14 19:37:12 -0400

    initial commit

---
+++ mod/JunimoServer/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace JunimoServer
+{
+    internal class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+
+}
---
+++ mod/JunimoServer/Services/GameCreatorService.cs
@@ -0,0 +1,29 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System;
+
+namespace JunimoServer.Services
+{
+    public record NewGameConfig(int WhichFarm = 0, bool UseSeperateWallets = false, int StartingCabins = 1);
+
+    class GameCreatorService
+    {
+
+        private IModHelper helper;
+        public GameCreatorService(IModHelper helper) {
+            this.helper = helper;
+        }
+
+        public void createNewGame(NewGameConfig config) {
+
+            Game1.player.team.useSeparateWallets.Value = config.UseSeperateWallets;
+            Game1.startingCabins = config.StartingCabins;
+            Game1.whichFarm = config.WhichFarm;
+            bool isWildernessFarm = config.WhichFarm == 4;
+            if (isWildernessFarm) {
+
+            }
+        }
+
+    }
+}
