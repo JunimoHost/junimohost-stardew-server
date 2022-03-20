b1318629d2fa99a1ed9a13075b4059a1360684f2
Evan Pierce <mrthinger@gmail.com>
2022-03-20 15:03:31 -0400

    add Backups

---
+++ mod/JunimoServer/Services/Backup/BackupScheduler.cs
@@ -0,0 +1,49 @@
+﻿using JunimoServer.Util;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace JunimoServer.Services.Backup
+{
+    public class BackupScheduler
+    {
+        private readonly BackupService _backupService;
+        private readonly IModHelper _modHelper;
+        private readonly IMonitor _monitor;
+
+        private const string BackupCreating = "Creating Backup...";
+        private const string BackupSuccess = "Backup Successful";
+        private const string BackupFail = "Backup Failed";
+
+
+        public BackupScheduler(IModHelper modHelper, BackupService backupService, IMonitor monitor)
+        {
+            _backupService = backupService;
+            _monitor = monitor;
+            _modHelper = modHelper;
+
+            modHelper.Events.GameLoop.SaveCreated += OnSaveCreated;
+            modHelper.Events.GameLoop.Saved += OnSaveCreated;
+        }
+
+        private void OnSaveCreated(object sender, SavedEventArgs e)
+        {
+            CreateBackup();
+        }
+
+        private void OnSaveCreated(object sender, SaveCreatedEventArgs e)
+        {
+            CreateBackup();
+        }
+
+        private void CreateBackup()
+        {
+            _monitor.Log(BackupCreating, LogLevel.Info);
+            _modHelper.SendPublicMessage(BackupCreating);
+            var success = _backupService.CreateBackupForCurrentDaySync();
+
+            var backupStatus = success ? BackupSuccess : BackupFail;
+            _modHelper.SendPublicMessage(backupStatus);
+            _monitor.Log(backupStatus, LogLevel.Info);
+        }
+    }
+}
---
+++ mod/JunimoServer/Services/Backup/BackupService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace JunimoServer.Services.Backup
+{
+    public class CreateBackupRequest
+    {
+        public BackupDate Date { get; set; }
+    }
+
+    public class BackupDate
+    {
+        public int Day { get; set; }
+        public int Season { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class BackupService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMonitor _monitor;
+
+        public BackupService(HttpClient httpClient, IMonitor monitor)
+        {
+            _httpClient = httpClient;
+            _monitor = monitor;
+        }
+
+        public bool CreateBackupForCurrentDaySync()
+        {
+            var dateNow = SDate.Now();
+            var request = new CreateBackupRequest
+            {
+                Date = new BackupDate
+                {
+                    Day = dateNow.Day,
+                    Season = dateNow.SeasonIndex,
+                    Year = dateNow.Year
+                }
+            };
+
+
+            try
+            {
+                var response = _httpClient.PostAsJsonAsync("/backup", request);
+                response.Wait();
+                return response.Result.StatusCode == HttpStatusCode.OK;
+            }
+            catch (Exception e)
+            {
+                _monitor.Log(e.ToString(), LogLevel.Error);
+                return false;
+            }
+        }
+    }
+}
