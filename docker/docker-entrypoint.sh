d561ac822fef7dbaa64d918cb5a694662b095523
Evan Pierce <mrthinger@gmail.com>
2022-08-07 23:29:30 +0100

    add sound and optimize docker image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 /opt/game-daemon &
 
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/startup)" != "200" ]]; do sleep 5; done'
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/startup)" != "200" ]]; do sleep 5; done
 
-/opt/tail-smapi-log.sh &
+tail -F /config/xdg/config/StardewValley/ErrorLogs/SMAPI-latest.txt &
 
 # wait until game-daemon says its ok to start stardew
 
 export XAUTHORITY=~/.Xauthority
-TERM=
-sed -i -e 's/env TERM=xterm $LAUNCHER "$@"$/env SHELL=\/bin\/bash TERM=xterm xterm  -e "\/bin\/bash -c $LAUNCHER "$@""/' /data/Stardew/Stardew\ Valley/StardewValley
+HOME=/root pulseaudio -vvv --daemonize
 
 bash -c "/data/Stardew/Stardew\ Valley/StardewValley"
-
-sleep 233333333333333
