e86a481570dbf3e1e87f0c0348dafd0891b09c95
Evan Pierce <mrthinger@gmail.com>
2022-08-17 12:31:10 -0400

    remove big sound

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export HOME=/root
+
 /opt/game-daemon &
 
+# wait until game-daemon says its ok to start stardew
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/startup)" != "200" ]]; do sleep 5; done
 
 tail -F /config/xdg/config/StardewValley/ErrorLogs/SMAPI-latest.txt &
-
-# wait until game-daemon says its ok to start stardew
 
 export XAUTHORITY=~/.Xauthority
 HOME=/root pulseaudio -vvv --daemonize
