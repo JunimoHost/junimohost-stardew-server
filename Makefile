189d20b4f33b8dc533d4e01a014dc2b88bf35b0f
Evan Pierce <mrthinger@gmail.com>
2024-01-12 22:20:56 -0800

    working inefficient instant resource pickup

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=v0.20.6
+VERSION=v0.20.7
 
 build: docker/mods/JunimoServer $(shell find docker -type f)
 	docker build --platform=amd64 -t gcr.io/junimo-host/stardew-base:$(VERSION) -f docker/Dockerfile .
--- mod/JunimoServer/Services/DebrisOptim/DebrisOptimizerOverrides.cs
+++ mod/JunimoServer/Services/DebrisOptim/DebrisOptimizerOverrides.cs
@@ -12,7 +12,9 @@ namespace JunimoServer.Services.DebrisOptim
         private static IModHelper _helper;
         private static Queue<DebrisLocation> _debrisQueue = new Queue<DebrisLocation>();
         private static HashSet<Debris> _debrisSet = new HashSet<Debris>();
-        private static int _maxUpdatesPerTick = 2;
+        private static int _maxUpdatesPerTick = 1;
+        private static int _updatesThisTick = 0;
+        private static Multiplayer _multiplayer;
 
         public class DebrisLocation
         {
@@ -33,17 +35,93 @@ namespace JunimoServer.Services.DebrisOptim
             _helper = helper;
         }
 
-        // ReSharper disable once InconsistentNaming
+        // ReSharper disable twice InconsistentNaming
         public static bool updateChunks_Prefix(Debris __instance, GameTime time, GameLocation location,
             ref bool __result)
         {
-            if (__instance.debrisType.Value != Debris.DebrisType.RESOURCE) return true;
-
-            __result = false;
-            if (!_debrisSet.Contains(__instance))
+            if (__instance.debrisType.Value != Debris.DebrisType.RESOURCE)
             {
-                _debrisQueue.Enqueue(new DebrisLocation(__instance, location));
-                _debrisSet.Add(__instance);
+                return true;
+            }
+
+            if (_updatesThisTick >= _maxUpdatesPerTick)
+            {
+                __result = false;
+                return false;
+            }
+
+
+            Vector2 debrisPos = new Vector2();
+            foreach (Chunk chunk in __instance.Chunks)
+            {
+                debrisPos += chunk.position.Value;
+            }
+
+            debrisPos /= __instance.Chunks.Count;
+
+            Farmer closestFarmer = null;
+            var closestDistance = float.MaxValue;
+            foreach (Farmer farmer in location.farmers)
+            {
+                var distance = (farmer.Position - debrisPos).LengthSquared();
+                if (distance < closestDistance)
+                {
+                    closestFarmer = farmer;
+                    closestDistance = distance;
+                }
+            }
+
+            if (closestFarmer == null)
+            {
+                __result = false;
+                return false;
+            }
+
+            var added = false;
+            _multiplayer ??= _helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
+
+            for (int index = __instance.Chunks.Count - 1; index >= 0; --index)
+            {
+                var chunk = __instance.Chunks[index];
+
+                int itemId =
+                    __instance.debrisType.Value is Debris.DebrisType.ARCHAEOLOGY or Debris.DebrisType.OBJECT
+                        ? chunk.debrisType
+                        : chunk.debrisType - chunk.debrisType % 2;
+
+                Item item;
+                if (chunk.debrisType != 93 && chunk.debrisType != 94)
+
+                    item = new Object(Vector2.Zero, itemId, 1)
+                    {
+                        Quality = __instance.itemQuality,
+                    };
+                else
+                    item = new Torch(Vector2.Zero, 1, itemId);
+
+                var farmerCanAccept = closestFarmer.couldInventoryAcceptThisItem(item);
+                if (farmerCanAccept)
+                {
+                    closestFarmer.addItemToInventory(item);
+                    __instance.Chunks.RemoveAt(index);
+                    added = true;
+                    //TODO: keep track of whos inventory was changed and batch it so farmer delta broadcasting is done in Update()
+                }
+            }
+
+            _updatesThisTick++;
+            __result = __instance.Chunks.Count == 0;
+
+            if (added)
+            {
+                var farmerRoot = _multiplayer.farmerRoot(closestFarmer.UniqueMultiplayerID);
+                var delta = _multiplayer.writeObjectDeltaBytes(farmerRoot);
+                foreach (var otherFarmer in Game1.otherFarmers)
+                {
+                    if (otherFarmer.Value.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
+                        otherFarmer.Value.queueMessage(0, closestFarmer,
+                            closestFarmer.UniqueMultiplayerID, delta);
+                }
             }
 
             return false;
@@ -51,77 +129,7 @@ namespace JunimoServer.Services.DebrisOptim
 
         public static void Update()
         {
-            int updatesThisTick = 0;
-
-            while (_debrisQueue.Count > 0 && updatesThisTick < _maxUpdatesPerTick)
-            {
-                var debrisLocation = _debrisQueue.Dequeue();
-                var debris = debrisLocation.Debris;
-                var location = debrisLocation.Location;
-                _debrisSet.Remove(debris);
-
-                Vector2 debrisPos = new Vector2();
-                foreach (Chunk chunk in debris.Chunks)
-                {
-                    debrisPos += chunk.position.Value;
-                }
-
-                debrisPos /= debris.Chunks.Count;
-
-                Farmer closestFarmer = null;
-                var closestDistance = float.MaxValue;
-                foreach (Farmer farmer in location.farmers)
-                {
-                    var distance = (farmer.Position - debrisPos).LengthSquared();
-                    if (distance < closestDistance)
-                    {
-                        closestFarmer = farmer;
-                        closestDistance = distance;
-                    }
-                }
-
-                if (closestFarmer == null)
-                {
-                    if (!_debrisSet.Contains(debris))
-                    {
-                        _debrisQueue.Enqueue(new DebrisLocation(debris, location));
-                        _debrisSet.Add(debris);
-                    }
-
-                    return;
-                }
-
-
-                for (int index = debris.Chunks.Count - 1; index >= 0; --index)
-                {
-                    var chunk = debris.Chunks[index];
-
-                    int itemId =
-                        (Debris.DebrisType)(NetFieldBase<Debris.DebrisType, NetEnum<Debris.DebrisType>>)debris
-                            .debrisType == Debris.DebrisType.ARCHAEOLOGY ||
-                        (Debris.DebrisType)(NetFieldBase<Debris.DebrisType, NetEnum<Debris.DebrisType>>)debris
-                            .debrisType == Debris.DebrisType.OBJECT
-                            ? chunk.debrisType
-                            : chunk.debrisType - chunk.debrisType % 2;
-
-                    Item item;
-                    if (chunk.debrisType != 93 && chunk.debrisType != 94)
-                        item = new Object(Vector2.Zero, itemId, 1)
-                        {
-                            Quality = debris.itemQuality,
-                        };
-                    else
-                        item = new Torch(Vector2.Zero, 1, itemId);
-                    var farmerCanAccept =
-                        closestFarmer.couldInventoryAcceptThisItem(item);
-                    if (farmerCanAccept)
-                    {
-                        closestFarmer.addItemToInventory(item);
-                    }
-                }
-
-                updatesThisTick++;
-            }
+            _updatesThisTick = 0;
         }
     }
 }
